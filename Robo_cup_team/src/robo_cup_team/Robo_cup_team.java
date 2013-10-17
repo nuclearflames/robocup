a906b3a5ad7d8997c1e497ea07875178a97a1e95
nuclearflames <1993jamesgrant@gmail.com>
2013-10-17 12:55:48 +0100

    project for robocup team
    edit upon this project

---
+++ Robo_cup_team/src/robo_cup_team/Robo_cup_team.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robo_cup_team;
+
+/**
+ *
+ * @author abnd175
+ */
+public class Robo_cup_team {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
